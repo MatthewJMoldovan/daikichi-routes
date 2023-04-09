79d1f0f2d8fe1b3f235b45e2f9857e8afa2a3413
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-04-09 14:15:04 -0600

    daikichi-routes

---
+++ src/main/java/com/mattmoldovan/daikichiroutes/controllers/DaiKichiController.java
@@ -0,0 +1,24 @@
+package com.mattmoldovan.daikichiroutes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DaiKichiController {
+	
+	@GetMapping("/daikichi")
+	public String daikichi() {
+		return"Welcome!";
+	}
+	
+	@GetMapping("daikichi/today")
+	public String today() {
+		return"Today you will find luck in all your endeavors!";
+	}
+	
+	@GetMapping("daikichi/tomorrow")
+	public String tomorrow() {
+		return"Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+}
---
+++ src/test/java/com/mattmoldovan/daikichiroutes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattmoldovan.daikichiroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
