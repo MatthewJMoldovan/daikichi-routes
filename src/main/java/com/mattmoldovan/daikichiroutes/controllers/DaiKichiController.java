98a3d2487443145392fc74bded516ea085415040
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-04-09 15:14:21 -0600

    Daikichi Path Variables

--- src/main/java/com/mattmoldovan/daikichiroutes/controllers/DaiKichiController.java
+++ src/main/java/com/mattmoldovan/daikichiroutes/controllers/DaiKichiController.java
@@ -1,6 +1,7 @@
 package com.mattmoldovan.daikichiroutes.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,14 +12,31 @@ public class DaiKichiController {
 		return"Welcome!";
 	}
 	
-	@GetMapping("daikichi/today")
+	@GetMapping("/daikichi/today")
 	public String today() {
 		return"Today you will find luck in all your endeavors!";
 	}
 	
-	@GetMapping("daikichi/tomorrow")
+	@GetMapping("/daikichi/tomorrow")
 	public String tomorrow() {
 		return"Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@GetMapping("/daikichi/{travel}")
+	public String travel(
+			@PathVariable("travel")String travel)
+	{
+	return "Congratulations! You will soon travel to " + travel;	
+	}
+	
+	@GetMapping("daikichi/lotto/{lotto}")
+	public String lotto(
+			@PathVariable("lotto")Integer lotto)
+	{
+		if (lotto % 2 == 1) {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+		}
+		return "You will take a grand journey in the near future, but be wary of temping offers";
 	}
 
 }
